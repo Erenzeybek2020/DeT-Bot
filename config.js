fc7b06b434968cd194e06ab346ed805089d06671
DEG <105592273+RextyIsAlive@users.noreply.github.com>
2023-08-18 19:13:36 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    prefix: "r!",
-    botdavet: "https://discord.com/api/oauth2/authorize?client_id=1014500672919584840&permissions=8&scope=bot%20applications.commands",
-    desteksunucusu: "https://discord.gg/sBwcePe77a",
-    website: "https://cot-bot-sitesi.glitch.me",
-    topgg: "https://top.gg/bot/1014500672919584840",
-    politika: "https://google.com",
-    sunucuid: "1014604925587705966",
-    destekçi: "1016826471668514937"
+    prefix: "",
+    botdavet: "",
+    desteksunucusu: "",
+    website: "",
+    topgg: "",
+    politika: "",
+    sunucuid: "",
+    destekçi: ""
 }
 
